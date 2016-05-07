6ecd6a0fd4fa9995511ae278d1e9f02e4f72732d
knoxor <paul@knoxor.com>
2016-05-07 14:28:00 +0100

    Create install-git.sh

---
+++ install-git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  Equip basec/core libs
+# Licence: MIT
+
+sudo apt-get update -y
+sudo apt-get install software-properties-common python-software-properties -y
+sudo apt-get install git -y
